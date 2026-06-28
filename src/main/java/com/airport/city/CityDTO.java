3806b03de76e01dada0d044d0c1ae0fae686d68a
Justin Seaward <jut_seaward@yahoo.com>
2026-06-28 15:24:57 -0230

    CityDTO: created city dto with variables, getters and setters to match JSON response from the city entity in the API

---
+++ src/main/java/com/airport/city/CityDTO.java
@@ -0,0 +1,54 @@
+package com.airport.city;
+
+public class CityDTO {
+
+    private Long id;
+    private String name;
+    private String state;
+    private int population;
+
+    public CityDTO(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    @Override
+    public String toString() {
+        return "CityDTO - " +
+                "Id: " + id +
+                ", Name: " + name + '\'' +
+                ", State: " + state + '\'' +
+                ", Population:" + population;
+    }
+}
