81c07c965741d19d737a8a5c324bf6ea36151cc8
Chris <Britten63@hotmail.com>
2026-06-30 18:39:52 -0300

    fixed passenger dto airport call on for Qeustion 2, and added getter setters

--- src/main/java/com/airport/passenger/PassengerDTO.java
+++ src/main/java/com/airport/passenger/PassengerDTO.java
@@ -12,5 +12,55 @@ public class PassengerDTO {
     private String firstName;
     private String lastName;
     private String phoneNumber;
+
+    // the aircraft list is what answers our documentation uqestion for number 2
+    // (aircraft per passenger)
+
     private List<AircraftDTO> aircraft;
 
+
+
+
+    public PassengerDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public List<AircraftDTO> getAircraft() {
+        return aircraft;
+    }
+
+    public void setAircraft(List<AircraftDTO> aircraft) {
+        this.aircraft = aircraft;
+    }
+}
