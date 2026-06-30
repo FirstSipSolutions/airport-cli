4c30fa3f0ac0d0e899d6a8d87c330312bc659bb9
Chris <Britten63@hotmail.com>
2026-06-30 11:24:33 -0300

    added variables to DTO for pass

--- src/main/java/com/airport/passenger/PassengerDTO.java
+++ src/main/java/com/airport/passenger/PassengerDTO.java
@@ -1,4 +1,16 @@
 package com.airport.passenger;
 
+import com.airport.aircraft.AircraftDTO;
+
+import java.util.List;
+
 public class PassengerDTO {
-}
+
+    // this is all taken from the DB via the API
+    // using Long for Id and String for the rest
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private List<AircraftDTO> aircraft;
+
