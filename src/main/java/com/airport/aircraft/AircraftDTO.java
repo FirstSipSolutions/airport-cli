269eaa45bf999e704003522e300ee7d1eb4c5b4f
Chris <Britten63@hotmail.com>
2026-07-01 10:43:05 -0300

    finished aircraftDTO

--- src/main/java/com/airport/aircraft/AircraftDTO.java
+++ src/main/java/com/airport/aircraft/AircraftDTO.java
@@ -29,3 +29,43 @@ public class AircraftDTO {
     public Long getId() {
         return id;
     };
+
+    // the rest of the
+    //getts setters here
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAirlineName() {
+        return airlineName;
+    }
+
+    public void setAirlineName(String airlineName) {
+        this.airlineName = airlineName;
+    }
+
+    public int getNumberOfPassengers() {
+        return numberOfPassengers;
+    }
+
+    public void setNumberOfPassengers(int numberOfPassengers) {
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    public List<AirportDTO> getAirports() {
+        return airports;
+    }
+
+    public void setAirports(List<AirportDTO> airports) {
+        this.airports = airports;
+    }
+}
