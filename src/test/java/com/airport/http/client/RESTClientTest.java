90cefb9443fae1e5ad3ba4d3923469d207217802
Chris <Britten63@hotmail.com>
2026-07-02 12:32:46 -0300

    added variables to rest client tests

--- src/test/java/com/airport/http/client/RESTClientTest.java
+++ src/test/java/com/airport/http/client/RESTClientTest.java
@@ -8,9 +8,17 @@
 
 package com.airport.http.client;
 
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
+
 public class RESTClientTest {
 
+// testing variables that will be needed
 
+    private HttpClient = mockHttpClient;
+    private RESTClient = restClient;
+
+    private HttpResponse<String> mockResponse;
 
 }
 
